cb1962c21ffecbb010a898568791aa43b43f790c
Matheus Silveira Ribeiro <103291130+msilveirar@users.noreply.github.com>
2022-06-04 12:58:46 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,35 @@
+const form = document.getElementById("Form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const passwordConfirmation = document.getElementById("password-confirmation");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  checkInputs();
+});
+
+function checkInputs() {
+  const usernameValue = username.value;
+  const emailValue = email.value;
+  const passwordValue = password.value;
+  const passwordConfirmationValue = passwordConfirmation.value;
+  if ((usernameValue = "")) {
+    setErrorFor(username, "O nome de usuário é obrigatório");
+  } else {
+    setSuccessFor(username);
+  }
+}
+
+function setErrorFor(input, message) {
+  const formControl = input.parentElement;
+  const small = formControl.querySelector("small");
+  small.innerText = message;
+  formControl.className = "form-control error";
+}
+
+function setSuccessFor(input) {
+  const formControl = input.parentElement;
+
+  formControl.className = "form-control success";
+}
