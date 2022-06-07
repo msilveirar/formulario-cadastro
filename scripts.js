324dbcbbbf6d4d3d4563eb5bb5aded3ce1a6e550
Matheus Silveira Ribeiro <103291130+msilveirar@users.noreply.github.com>
2022-06-07 13:17:43 -0300

    Add files via upload
    formulário finalizado.

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-const form = document.getElementById("Form");
+const form = document.getElementById("form");
 const username = document.getElementById("username");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
@@ -14,10 +14,38 @@ function checkInputs() {
   const emailValue = email.value;
   const passwordValue = password.value;
   const passwordConfirmationValue = passwordConfirmation.value;
-  if ((usernameValue = "")) {
+  if (usernameValue === "") {
     setErrorFor(username, "O nome de usuário é obrigatório");
   } else {
     setSuccessFor(username);
+  }
+  if (emailValue === "") {
+    setErrorFor(email, "O email é obrigatório.");
+  } else if (!checkEmail(emailValue)) {
+    setErrorFor(email, "Por favor, insira um email válido.");
+  } else {
+    setSuccessFor(email);
+  }
+  if (passwordValue === "") {
+    setErrorFor(passaword, "A senha é obrigatória.");
+  } else if (passwordValue.length < 7) {
+    setErrorFor(password, "A senha deve ter no mínimo 7 caracteres.");
+  } else {
+    setSuccessFor(password);
+  }
+  if (passwordConfirmationValue === "") {
+    setErrorFor(passwordConfirmation, "A confirmação de senha é obrigatória.");
+  } else if (passwordConfirmationValue !== passwordValue) {
+    setErrorFor(passwordConfirmation, "As senhas devem ser iguais.");
+  } else {
+    setSuccessFor(passwordConfirmation);
+  }
+  const formControls = form.querySelectorAll(".form-control");
+  const formIsValid = [...formControls].every((formControl) => {
+    return formControl.className === "form-control success";
+  });
+  if (formIsValid) {
+    console.log("O formulário está 100% válido!");
   }
 }
 
@@ -32,4 +60,10 @@ function setSuccessFor(input) {
   const formControl = input.parentElement;
 
   formControl.className = "form-control success";
+}
+
+function checkEmail(email) {
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    email
+  );
 }
